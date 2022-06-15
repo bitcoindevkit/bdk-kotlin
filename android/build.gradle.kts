4b2ef8b9403537746f9b32ffe7c69c8907f5c43b
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-06-15 17:01:42 -0300

    Bump version to 0.7.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "org.bitcoindevkit"
                 artifactId = "bdk-android"
-                version = "0.7.0-SNAPSHOT"
+                version = "0.7.0"
                 from(components["release"])
                 pom {
                     name.set("bdk-android")
@@ -103,7 +103,7 @@ tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
     dokkaSourceSets {
         named("main") {
             moduleName.set("bdk-android")
-            moduleVersion.set("0.7.0-SNAPSHOT")
+            moduleVersion.set("0.7.0")
             includes.from("Module.md")
         }
     }
--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
 
                 groupId = "org.bitcoindevkit"
                 artifactId = "bdk-jvm"
-                version = "0.7.0-SNAPSHOT"
+                version = "0.7.0"
 
                 from(components["java"])
 
@@ -101,7 +101,7 @@ tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
     dokkaSourceSets {
         named("main") {
             moduleName.set("bdk-jvm")
-            moduleVersion.set("0.7.0-SNAPSHOT")
+            moduleVersion.set("0.7.0")
             includes.from("Module.md")
         }
     }
